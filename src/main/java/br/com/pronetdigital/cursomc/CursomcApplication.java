9b8bcc8d670d781b4cac7d3a8507565ad766259f
Eliel Sobral <eliel.sobral@gmail.com>
2021-03-28 16:14:10 -0300

    Criando domain Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/pronetdigital/cursomc/CursomcApplication.java
+++ src/main/java/br/com/pronetdigital/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package br.com.pronetdigital.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.pronetdigital.cursomc.domain.Cidade;
 import br.com.pronetdigital.cursomc.domain.Cliente;
 import br.com.pronetdigital.cursomc.domain.Endereco;
 import br.com.pronetdigital.cursomc.domain.Estado;
+import br.com.pronetdigital.cursomc.domain.Pagamento;
+import br.com.pronetdigital.cursomc.domain.PagamentoComBoleto;
+import br.com.pronetdigital.cursomc.domain.PagamentoComCartao;
+import br.com.pronetdigital.cursomc.domain.Pedido;
 import br.com.pronetdigital.cursomc.domain.Produto;
+import br.com.pronetdigital.cursomc.domain.enums.EstadoPagamento;
 import br.com.pronetdigital.cursomc.domain.enums.TipoCliente;
 import br.com.pronetdigital.cursomc.repositories.CategoriaRepository;
 import br.com.pronetdigital.cursomc.repositories.CidadeRepository;
 import br.com.pronetdigital.cursomc.repositories.ClienteRepository;
 import br.com.pronetdigital.cursomc.repositories.EnderecoRepository;
 import br.com.pronetdigital.cursomc.repositories.EstadoRepository;
+import br.com.pronetdigital.cursomc.repositories.PagamentoRepository;
+import br.com.pronetdigital.cursomc.repositories.PedidoRepository;
 import br.com.pronetdigital.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,12 +49,16 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -91,6 +103,22 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
---
+++ src/main/java/br/com/pronetdigital/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package br.com.pronetdigital.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.pronetdigital.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
