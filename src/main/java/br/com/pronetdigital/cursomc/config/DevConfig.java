e3d76ee68de81331e401bba59e1efe1fc576ec81
Eliel Sobral <eliel.sobral@gmail.com>
2021-03-29 21:18:53 -0300

    Enviando Email no formato HTML

--- src/main/java/br/com/pronetdigital/cursomc/config/DevConfig.java
+++ src/main/java/br/com/pronetdigital/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.pronetdigital.cursomc.services.DBService;
+import br.com.pronetdigital.cursomc.services.EmailService;
+import br.com.pronetdigital.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,4 +32,10 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+
 }
